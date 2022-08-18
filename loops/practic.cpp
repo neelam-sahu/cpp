a2326f44f8b8fa271482462ad96fe7fb1c57eab4
neelam-sahu <neelamsahu1412@gmail.com>
2022-08-18 22:56:59 +0530

    learned different paterns and loops

---
+++ loops/practic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int count{10};
+
+  for (size_t i = 1; i < count; i++) // n - 1
+  {
+    cout << i;
+  }
+}
--- operators/test1.cpp
+++ operators/test1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
 
+  /*
   int i = 10, j = 20, k;
   //10     //9   //19   //20  //9   //20  //10  //19
   k = i-- - i++ + --j - ++j + --i - j-- + ++i - j++;
@@ -11,6 +12,6 @@ int main()
   cout << "i=" << i << endl;//10
   cout << "j=" << j << endl;//20
   cout << "k=" << k << endl;//-20
+  */
 
-  return 0;
 }
